3df15021ffcc22d99e5670d5ac378d4d4ad3c26c
Oliver <oliver.henry.walters@gmail.com>
2016-06-11 13:05:48 +1000

    Got relative includes working

---
+++ Code/Firmware/code/inc/box_usb.h
@@ -0,0 +1,19 @@
+#ifndef _BOX_USB_H
+#define _BOX_USB_H
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "ReverseGeocacheProtocol.h"
+#include "boxdefines.h"
+#include "box_defines.h"
+
+#include "types.h"
+
+bool Handle_Box_Message();
+
+extern HIDBuffer txBuffer;
+extern HIDBuffer rxBuffer;
+
+#endif
