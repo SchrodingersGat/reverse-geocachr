b455b30ca5d523063b2346c2ed33197631a0226e
Oliver <oliver.henry.walters@gmail.com>
2017-07-23 10:04:19 +1000

    Removed old GUI code

--- Code/GUI/ReverseGeocachr/debug.cpp
+++
@@ -1,9 +0,0 @@
-#include "debug.h"
-
-#define DEBUG_ON
-
-void Debug(QString msg) {
-#ifdef DEBUG_ON
-    qDebug() << msg;
-#endif
-}
--- Code/GUI/ReverseGeocachr/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-
-#include <QDebug>
-
-void Debug(QString msg);
-
-#endif // DEBUG_H
-
